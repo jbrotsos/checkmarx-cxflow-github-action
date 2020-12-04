1f6c309197f42817626444428630ffc6911665a9
Mithilesh Pawar <Mithilesh.Pawar@checkmarx.com>
2020-12-03 21:37:04 -0500

    Signed-off-by: Mithilesh Pawar <Mithilesh.Pawar@checkmarx.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -jar /app/cx-flow.jar --spring.profiles.active="${CXFLOW_ENABLED-VULNERABILITY-SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
+java -jar /app/cx-flow.jar --spring.profiles.active="${SCANNERS}" --scan --github.api-url="${GITHUB_API_URL}/repos/" --cx-team="${TEAM}" --cx-project="${PROJECT}" --app="${APP}" --f=. ${CXFLOW_PARAMS}
